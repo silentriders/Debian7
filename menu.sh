11b4f34e31d07054abd12dc7a289e33411effd61
silentriders <gilangixb1314@gmail.com>
2017-07-18 10:48:19 +0000

    Update menu.sh

--- menu.sh
+++ menu.sh
@@ -2,12 +2,12 @@
 #Menu
 
 echo -e "================================MENU====================================="
-echo -e "* menu       : senarai perintah"
+echo -e "* menu       : daftar perintah"
 echo -e "* usernew    : membuat akaun SSH"
 echo -e "* trial      : membuat akaun trial"
 echo -e "* hapus      : menghapus akaun SSH"
-echo -e "* login      : semak login user"
-echo -e "* member     : senarai user"
+echo -e "* login      : melihat login user"
+echo -e "* member     : daftar user"
 echo -e "* resvis     : restart service dropbear, webmin, squid3, openvpn dan ssh"
 echo -e "* reboot     : reboot VPS"
 echo -e "* speedtest  : speedtest VPS"
